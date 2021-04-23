829acfcbf2269c46530d81d0b388a8a54e0bef0e
Pranali216 <pranalikhilari21@gmail.com>
2021-04-23 16:57:38 +0530

    initial commit

---
+++ Input1.py
@@ -0,0 +1,15 @@
+x="2"
+y="4"
+z=int(x) + int(y)
+print(z)
+
+#***
+name=input()
+age=input()
+print(name +" is " +age)
+
+#addition of two numbers by taking input from user
+a= int(input())
+b=int(input())
+c=a+b
+print(c)
