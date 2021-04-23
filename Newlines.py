1304cf4481f382aa0bd2c8d184cc8dddf9f34b47
Pranali216 <pranalikhilari21@gmail.com>
2021-04-23 13:19:27 +0530

    initial commit

---
+++ Newlines.py
@@ -0,0 +1,11 @@
+
+#Newlines will be automatically added for strings that are created using three quotes.
+print("""this
+is a
+multiline
+text""")
+
+
+# Newlines can be given by\n
+print("Below is a text using backslash n \n\n")
+print("One \nTwo \nThree")
