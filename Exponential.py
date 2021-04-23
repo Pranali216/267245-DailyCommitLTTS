c90aab4da5785c00b9e409278c699de098a4c0e8
Pranali216 <pranalikhilari21@gmail.com>
2021-04-23 12:34:55 +0530

    initial commit

---
+++ Exponential.py
@@ -0,0 +1,7 @@
+# an exponention operator is denoted by double asterick **
+ # this denotes raised to
+
+print(2**3)    # 2 to the power 3 or 2 raised to 3
+print(9**(1/2))  # square root of 9
+print(8**(1/2))  #cube root of 8
+print(2**2**3)  # 2^2=4 ,4^3=256
