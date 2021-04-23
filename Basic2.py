e7981d80365d0df2007eab2731b243256e249f13
Pranali216 <pranalikhilari21@gmail.com>
2021-04-23 12:33:09 +0530

    initial commit

---
+++ Basic2.py
@@ -0,0 +1,15 @@
+#mathematical operations
+
+print(2+2)
+print(5+4-3)
+print(2*(3+4))
+print(10/2)
+# operation  int+int =float
+#operation  int +float=float
+#operation float+float=float
+
+print(3/4)
+print(0.42)
+print(8/2)
+print(6*7.0)
+print(4+1.65)
